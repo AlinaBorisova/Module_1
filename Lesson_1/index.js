8e3d7b4ee4e92d287a7b91619e1ad245bdecc0bc
Alina Borisova <b-alina1999.borisova@yandex.ru>
2022-02-20 19:25:23 +0300

    initial

---
+++ Lesson_1/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+{
+    const item = 'P40';
+    const count = 20; 
+    const category = 'Gadget';
+    const price = 700;
+
+    console.log('item:', item);
+    console.log(`Общая сумма товара: ${count * price}`);
+}
---
+++ Lesson_2/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+    {
+    const item = 'P40';
+    const count = 20; 
+    const category = 'Gadget';
+    const price = 700;
+
+    console.log('item:', item);
+    console.log(`Общая сумма товара: ${count * price}`);
+    }
+
+    {
+    const item = 'Freebuds pro';
+    let count = 10;
+    const category = 'gadget';
+    let price = 290;
+
+    console.log('item:', item)
+    console.log(`Общая сумма товара: ${count * price}`);
+    }
