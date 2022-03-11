c6c241c1c824715215a43396a19bfaa2a94dd604
Alina Borisova <b-alina1999.borisova@yandex.ru>
2022-03-11 17:27:42 +0300

    Корректировка

--- Lesson_8/Task03.js
+++ Lesson_8/Task03.js
@@ -11,15 +11,15 @@ const newGetRandom2 = (arr, n, m, str) => {
     max = n;
     min = m;
     };
-        for(let i = 0; i <= arr; i++) {
-            result.push(Math.ceil(Math.random() * ((max - min) + min)));
-        };
-
-        if (str = 'even') {
-            return result.filter((item) => (item % 2 === 0))
-        };
-        if (str = 'odd') {
-            return result.filter((item) => (item % 2 === 1))
-        };
+    for(let i = 0; i <= arr; i++) {
+        result.push(Math.ceil(Math.random() * ((max - min) + min)));
+    };
+    if (str === 'even') {
+        return result.filter((item) => (item % 2 === 0))
+    };
+    if (str === 'odd') {
+        return result.filter((item) => (item % 2 === 1))
+    };
 };
-    console.log(newGetRandom2(10, 100, 0, 'odd'));
+    console.log(newGetRandom2(10, 100, 0, 'even'));
+    
